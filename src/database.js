9f4ae35902f62fb7d34fe6f9bd3916dc908545aa
muuhab <mohab.barca@gmail.com>
2022-03-10 17:35:28 +0200

    Configure deploy

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv");
-const pg = require('pg')
+const pg = require("pg");
 
 dotenv.config();
 const {
@@ -8,14 +8,20 @@ const {
   POSTGRES_USER,
   POSTGRES_PASSWORD,
   POSTGRES_PORT,
+  NODE_ENV
 } = process.env;
 
-const client = new pg.Pool({
+const devConfig = {
   host: POSTGRES_HOST,
   database: POSTGRES_DB,
   user: POSTGRES_USER,
   port: POSTGRES_PORT,
   password: POSTGRES_PASSWORD,
-});
+};
+
+const prodConfig={
+  connectionString:process.env.DATABASE_URL,
+}
+const client = new pg.Pool(NODE_ENV==='production'?prodConfig:devConfig);
 
 module.exports = client;
